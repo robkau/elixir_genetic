bf5e5174dda2b51285e3afaa1766c7f6a9faee08
robkau <robkau@users.noreply.github.com>
2023-05-28 17:18:27 -0400

    p 31 - refactor to module with more general algorithm

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -1,61 +1,10 @@
-population = for _ <- 1..100, do: for(_ <- 1..1000, do: Enum.random(0..1))
+genotype = fn -> for _ <- 1..1000, do: Enum.random(0..1) end
 
-# Sort chromosomes by performance
-fitness = fn population ->
-  Enum.sort_by(population, &Enum.sum/1, &>=/2)
-end
+fitness_function = fn chromosome -> Enum.sum(chromosome) end
 
-# Take pairs of best chromosomes
-selection = fn population ->
-  population
-  |> Enum.chunk_every(2)
-  |> Enum.map(&List.to_tuple(&1))
-end
+max_fitness = 1000
 
-# For each chromosome pair, cut both chromosomes in half at random point
-# And then swap the tail parts
-crossover = fn population ->
-  Enum.reduce(population, [], fn {p1, p2}, acc ->
-    cx_point = :rand.uniform(1000)
-    {{h1, t1}, {h2, t2}} = {Enum.split(p1, cx_point), Enum.split(p2, cx_point)}
-    [h1 ++ t2, h2 ++ t1 | acc]
-  end)
-end
+soln = Genetic.run(fitness_function, genotype, max_fitness)
 
-# Each chromosome has a 5% chance to shuffle all genes
-mutation = fn population ->
-  population
-  |> Enum.map(fn chromosome ->
-    if :rand.uniform() < 0.05 do
-      Enum.shuffle(chromosome)
-    else
-      chromosome
-    end
-  end)
-end
-
-algorithm = fn population, algorithm ->
-  best = Enum.max_by(population, &Enum.sum/1)
-  IO.write("\rCurrent Best: " <> Integer.to_string(Enum.sum(best)))
-
-  if Enum.sum(best) == 1000 do
-    best
-  else
-    # Initial population
-    population
-    # Evaluate
-    |> fitness.()
-    # Select parents
-    |> selection.()
-    # Create children
-    |> crossover.()
-    # Random mutations
-    |> mutation.()
-    # Repeat with new population
-    |> algorithm.(algorithm)
-  end
-end
-
-solution = algorithm.(population, algorithm)
-IO.write("\n Answer is \n")
-IO.inspect(solution)
+IO.write("\n")
+IO.inspect(soln)
