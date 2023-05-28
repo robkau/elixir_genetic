827337b10dbc6edde15cf1522cda76493eaf9e08
robkau <robkau@users.noreply.github.com>
2023-05-28 19:40:55 -0400

    p 47 - refactor with Problem behaviour

---
+++ lib/problem.ex
@@ -0,0 +1,10 @@
+# To solve a problem using the genetic module, supply:
+# - a genotype (function to generate new chromosomes)
+# - a fitness function (to evaluate a given chromosome)
+# - an end point
+defmodule Problem do
+  alias Types.Chromosome
+  @callback genotype :: Chromosome.t()
+  @callback fitness_function(Chromosome.t()) :: number()
+  @callback terminate?(Enum.t()) :: boolean()
+end
--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -1,10 +1,24 @@
-genotype = fn -> for _ <- 1..1000, do: Enum.random(0..1) end
+defmodule OneMax do
+  @behaviour Problem
+  alias Types.Chromosome
 
-fitness_function = fn chromosome -> Enum.sum(chromosome) end
+  # Length of the bit array to maximize
+  @size 500
 
-max_fitness = 1000
+  @impl true
+  def genotype do
+    genes = for _ <- 1..@size, do: Enum.random(0..1)
+    %Chromosome{genes: genes, size: @size}
+  end
 
-soln = Genetic.run(fitness_function, genotype, max_fitness)
+  @impl true
+  def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
+
+  @impl true
+  def terminate?([best | _]), do: best.fitness == @size
+end
+
+soln = Genetic.run(OneMax)
 
 IO.write("\n")
 IO.inspect(soln)
